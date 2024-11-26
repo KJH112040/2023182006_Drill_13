34a0e07f0b26daf935e231c5222e0692543b8fe0
KJH04_ <10wjdgp04@gmail.com>
2024-11-26 10:57:37 +0900

    상하좌우 무한 스크롤링

--- boy.py
+++ boy.py
@@ -227,14 +227,15 @@ class Boy:
         # self.y=clamp(get_canvas_height()/2,self.y,server.background.h-get_canvas_height()/2)
 
         # 월드 좌표계를 화면 좌표로 변환
-        self.x = clamp(50.0,self.x,server.background.w - 50.0)
-        self.y = clamp(50.0,self.y,server.background.h - 50.0)
+        # self.x = clamp(50.0,self.x,server.background.w - 50.0)
+        # self.y = clamp(50.0,self.y,server.background.h - 50.0)
 
     def handle_event(self, event):
         self.state_machine.handle_event(('INPUT', event))
 
     def draw(self):
-        sx,sy=self.x-server.background.window_left,self.y-server.background.window_bottom
+        # sx,sy=self.x-server.background.window_left,self.y-server.background.window_bottom
+        sx,sy=get_canvas_width()//2,get_canvas_height()//2
         self.image.clip_draw(int(self.frame) * 100, self.action * 100, 100, 100, sx, sy)
         self.font.draw(sx - 100, sy + 60, f'({self.x:5.5}, {self.y:5.5})', (255, 255, 0))
 
